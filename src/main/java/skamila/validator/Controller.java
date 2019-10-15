b7a269bbac3af537f0ddd40b6668df2d92e9d040
Kamila <wiewiora145@gmail.com>
2019-10-15 17:25:14 +0200

    dodanie walidacji

--- src/main/java/skamila/validator/Controller.java
+++ src/main/java/skamila/validator/Controller.java
@@ -11,9 +11,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @RequestMapping(value ="/validate", method = RequestMethod.GET)
+    @RequestMapping(value = "/validate", method = RequestMethod.GET)
     public ResponseEntity rev(@RequestParam String s) {
 
-        return new ResponseEntity(s, new HttpHeaders(), HttpStatus.OK);
+        int upperLetterCounter = 0;
+        int loweLetterCounter = 0;
+        int digitCounter = 0;
+        int specialCharactersCounter = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+            if (Character.isUpperCase(c)) {
+                upperLetterCounter++;
+            } else if (Character.isLowerCase(c)) {
+                loweLetterCounter++;
+            } else if (Character.isDigit(c)) {
+                digitCounter++;
+            } else {
+                specialCharactersCounter++;
+            }
+
+        }
+
+        String result = "Przekazany ciąg " + s + " zawiera: " +
+                upperLetterCounter + " wielkich liter " +
+                loweLetterCounter + " małych liter " +
+                digitCounter + " cyfr i " +
+                specialCharactersCounter + " znaków specjalnych.";
+
+        return new ResponseEntity(result, new HttpHeaders(), HttpStatus.OK);
     }
 }
